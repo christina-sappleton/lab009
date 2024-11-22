7292fa256b7544a8eb2c65c7e2e0ace870710a23
christina-sappleton <cms85@njit.edu>
2024-11-22 09:29:39 -0500

    .

---
+++ scriptone.js
@@ -0,0 +1,63 @@
+let characters = ["Harry","Hermione","Ron","Draco","Luna"]
+    for(let i = 1; i <= 4;){
+        console.log("Harry");
+    }
+    for(let i = 2; i <= 13;){
+        console.log("Hermione");
+    }
+    for(let i = 3; i <= 17;){
+        console.log("Ron");
+    }
+    for(let i = 4; i <= 23;){
+        console.log("Draco");
+    }
+    for(let i = 5; i <= 28;){
+        console.log("Luna");
+    }
+
+    var firstName = "Harry"
+    let house = "Gryffindor"
+    {`string text ${Welcome, Harry of Gryffindor!}string text`}
+
+    let characterName = "Hermione"
+    str.toUpperCase("Hermione")
+    str.toLowerCase("Hermione")
+
+    let spell = "Expelliarmus"
+    str.trim("Expelliarmus")
+
+    let quote = "I solemnly swear that I am up to no good"
+    str.slice(11,15)
+
+    var firstName = "Ron"
+    let lastName = "Weasley"
+    Concat("Ron Weasley")
+
+    let sentence = "Draco is a good wizard"
+    str.replace("good","bad")
+
+    let houses = ["Gryffindor","Hufflepuff","Ravenclaw"];
+    Push("Slytherin")
+    Pop("Slytherin")
+
+    let spells = ["Alohomora","Lumos","Nox"];
+    unshift("Accio")
+    shift("Accio")
+
+    let professors = ["Dumbledore","Mcgonagall","Snape","Hagrid"];
+    slice(11, 26)
+
+    let students = ["Neville","Seamus","Dean","Parvati"]
+    Splice(0,13)
+
+    let phrase = "Mischief Managed"
+    str.trim("Mischief Managed")
+    str.toLowerCase("Mischief Managed")
+    concat("-Harry")
+
+    let wizards = ["Harry","Hermione","Ron"]
+    push("Luna","Draco")
+
+    let message = "Welcome to Hogwarts School of Witchcraft and Wizardry"
+    slice(12,19)
+    concat("Castle")
